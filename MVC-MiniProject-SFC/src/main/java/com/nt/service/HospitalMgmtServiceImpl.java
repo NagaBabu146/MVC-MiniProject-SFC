cadab58ab799f6dd41dd27b0a64e89eda8c2cec1
nagababu <b.nagababu146@gmail.com>
2019-12-27 22:46:04 +0530

    commiting once again

--- MVC-MiniProject-SFC/src/main/java/com/nt/service/HospitalMgmtServiceImpl.java
+++ MVC-MiniProject-SFC/src/main/java/com/nt/service/HospitalMgmtServiceImpl.java
@@ -1,24 +1,33 @@
 package com.nt.service;
+
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.BeanUtils;
 import com.nt.bo.PatientDetailsBO;
 import com.nt.dao.PatientDAO;
 import com.nt.dto.PatientDetailsDTO;
+
 public class HospitalMgmtServiceImpl implements HospitalMgmtService {
+	
 	private PatientDAO dao;
+	
 	public HospitalMgmtServiceImpl(PatientDAO dao) {
 		this.dao = dao;
 	}
+	
 	public HospitalMgmtServiceImpl() {
 		System.out.println("HospitalMgmtServiceImpl::0-param constructor");
 	}
+	
 	@Override
 	public List<PatientDetailsDTO> fetchAllPatients() {
+		
 		List<PatientDetailsBO> listBO = null;
 		List<PatientDetailsDTO> listDTO = new ArrayList();
+		
 		// use dao
 		listBO = dao.getAllPatients();
+		
 		// convert listBO to listDTO
 		listBO.forEach(bo -> {
 			PatientDetailsDTO dto = new PatientDetailsDTO();
@@ -28,11 +37,14 @@ public class HospitalMgmtServiceImpl implements HospitalMgmtService {
 		});
 		return listDTO;
 	}
+	
 	@Override
 	public PatientDetailsDTO fetchPatientById(int id) {
 		System.out.println("fetchById:in Service");
+		
 		PatientDetailsBO bo = null;
 		PatientDetailsDTO dto = null;
+		
 		dto = new PatientDetailsDTO();
 		// use dao
 		bo = dao.getPatientById(id);
@@ -40,13 +52,18 @@ public class HospitalMgmtServiceImpl implements HospitalMgmtService {
 		BeanUtils.copyProperties(bo, dto);
 		return dto;
 	}
+	
 	@Override
 	public String modifyPatient(PatientDetailsDTO dto) {
-		//convert dto to bo
+	
 		PatientDetailsBO bo=null;
 		bo=new PatientDetailsBO();
+		
+		//convert dto to bo
 		BeanUtils.copyProperties(dto, bo);
+		
 	int count=	dao.updatePatient(bo);
+	
 	if(count==0)
 		return "patient details are  not updated";
 	else
